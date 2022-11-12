24a27d0898953cf5d6d66e5b106c8e9d2b90a6d5
Alexandra Frolkova <frolkova_1996@mail.ru>
2022-11-12 18:02:01 +1000

    Implemented application logic

---
+++ domain/IAppointmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain
+{
+    public interface IAppointmentRepository
+    {
+        DateTime[]? GetFreeAppointmentDateByDrSpec(DrSpec spec);
+        Appointment MakeAppointmentOnSelectedDateSpecificDoctor(int id, DateTime selectedDate);
+        Appointment MakeAppointmentOnSelectedDateFreeDoctor(DateTime selectedDate, DrSpec spec);
+    }
+}
---
+++ domain/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace domain
+{
+    public interface IUserRepository
+    {
+        User? GetByLogin(string login);
+        bool? CheckUserExists(string login, string password);
+        User? CreateUser(NewUser newUser);
+
+    }
+}
